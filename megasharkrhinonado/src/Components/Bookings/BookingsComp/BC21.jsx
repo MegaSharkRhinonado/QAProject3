7b2470256b065ce9727f2595f1b35a36d1791f86
James <2jcjl4@gmail.com>
2022-06-22 00:10:35 +0100

    MEG-31 #React is done for Bookings just needs CSS and database linking

---
+++ megasharkrhinonado/src/Components/Bookings/BookingsComp/BC21.jsx
@@ -0,0 +1,20 @@
+
+const BC21 = () => {
+    return ( 
+        <>
+        {/* might have to add value="" to this, not sure on how to structor this part */}
+        {/* would also be worth importing a image and altering what the seats look like rather than a checkbox */}
+        <input type="checkbox" id="seat1" name="seat" />
+        <input type="checkbox" id="seat2" name="seat" />
+        <input type="checkbox" id="seat3" name="seat" />
+        <input type="checkbox" id="seat4" name="seat" />
+        <input type="checkbox" id="seat5" name="seat" />
+        <input type="checkbox" id="seat6" name="seat" />
+        <input type="checkbox" id="seat7" name="seat" />
+        <input type="checkbox" id="seat8" name="seat" />
+        <input type="checkbox" id="seat9" name="seat" />
+       </>
+     );
+}
+
+export default BC21;
