305abf68a5eed9cfe67a8739e782c6932c9a6288
Jesse <jesse.morgan@hotmail.co.uk>
2022-06-21 16:37:17 +0100

    MEG-14 #JM: NavBar started

--- megasharkrhinonado/src/Components/HeaderAndFooter/navBar.jsx
+++ megasharkrhinonado/src/Components/HeaderAndFooter/navBar.jsx
@@ -1,7 +1,23 @@
+import { Link } from 'react-router-dom'
+import React from 'react';
+import logo from '../../Media/Nav/LOGO.png';
 const NavBar = () => {
     return ( 
         <>
-            <h2>NavBar</h2>
+            <div className="headContainer">
+                <h2>QA Cinemas</h2>
+                <Link to="/"><img src={logo} alt="LOGO"></img></Link>
+                <div className="navContainer">
+                    <button type="button"><Link exact to="/">Home</Link></button>
+                    <button type="button"><Link to="/ListingGallery">Listings</Link></button>
+                    <button type="button"><Link to="/Bookings">Bookings</Link></button>
+                    <button type="button"><Link to="/Classifications">Classifications</Link></button>
+                    <button type="button"><Link to="/Screens">Screens</Link></button>
+                    <button type="button"><Link to="/NewReleases">New Releases</Link></button>
+                    <button type="button"><Link to="/Forums">Forum</Link></button>
+                </div>
+            </div >
+
         </>
      );
 }
