b23ba5dd2e95562876784b2597fcb6977c59918b
elistribley <eli_stribley@hotmail.com>
2022-06-22 11:59:18 +0100

    MEG-24 #ES: BookingCon react done

---
+++ megasharkrhinonado/src/Components/BookingsConfirmation/BookingsConCards/BCConcard01.jsx
@@ -0,0 +1,12 @@
+
+import BCCon11 from "../BookingsConComp/BCCon11"
+
+const BCConcard01 = () => {
+    return ( 
+        <>
+        <BCCon11 />
+        </>
+     );
+}
+ 
+export default BCConcard01;
