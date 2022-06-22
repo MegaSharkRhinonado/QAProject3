5447459dffd9acaa0b45a247da3d5b09bdd76e7d
elistribley <eli_stribley@hotmail.com>
2022-06-22 13:48:42 +0100

    MEG-122 #ES: Forums part 2 divs seperating discussions

--- megasharkrhinonado/src/Components/Forum/ForumsComp/ForumComp02.jsx
+++ megasharkrhinonado/src/Components/Forum/ForumsComp/ForumComp02.jsx
@@ -4,14 +4,22 @@ const ForumComp02 = () => {
         <div>
         <div>
             <h2>Discussion Board</h2>
+            <div id="disOne">
             <p>Text</p>
             <input type="text"></input>
+            </div>
+            <div id="disTwo">
             <p>Text</p>
             <input type="text"></input>
+            </div>
+            <div id="disThree">
             <p>Text</p>
             <input type="text"></input>
+            </div>
+            <div id="disFour">
             <p>Text</p>
             <input type="text"></input>
+            </div>
            </div>
         </div>
         </>
