92ee8a0d299805aa440ba91c49d533b5cfa53dbe
Matthew Duck <MatthewDDuck@googlemail.com>
2022-06-23 12:02:05 +0100

    MEG-21 #MD: Fixed infinite get request

--- megasharkrhinonado/src/Components/NewReleases/newReleases.jsx
+++ megasharkrhinonado/src/Components/NewReleases/newReleases.jsx
@@ -3,21 +3,27 @@ import axios from 'axios';
 import React, { useEffect, useState } from "react";
 
 const NewReleases = () => {
-    
+
     const [newReleases, setNewReleases] = useState([]);
 
-    axios.get("http://localhost:3000/movies/getAll")
-    .then(response => {
-        setNewReleases(response.data)
-    }).catch((exception) => {
-        console.log(exception);
-    });
+    const getReleases = () => {
+        axios.get("http://localhost:3000/movies/getAll")
+            .then(response => {
+                setNewReleases(response.data)
+            }).catch((exception) => {
+                console.log(exception);
+            });
+    }
+
+    useEffect(() => {
+        getReleases();
+    }, []);
 
     return (
         <>
             {
-                newReleases.map(newRelease => 
-                 <NRC01 data={newRelease} key={newRelease.id} />
+                newReleases.map(newRelease =>
+                    <NRC01 data={newRelease} key={newRelease.id} />
                 )
             }
         </>
