302cf8b35534a625dab64b1ceae7e68f1c88b9d5
elistribley <eli_stribley@hotmail.com>
2022-06-24 12:52:47 +0100

    MEG-68 #ES: updated css

--- megasharkrhinonado/src/Components/PlacesToGo/PTGComp/PTGComp03.jsx
+++ megasharkrhinonado/src/Components/PlacesToGo/PTGComp/PTGComp03.jsx
@@ -4,10 +4,10 @@ const PTGComp03 = () => {
     return (
         <>
             <div >
-                <h2>Lothloriens Forest</h2>
+                <h2>Lothlorien Forest</h2>
                 <img src={image03} alt="Caras Galadhon" width="370px" />
                 <h4>Fancy stopping off at a magical forest? Galadriel has provided just that for you!</h4>
-                <h4>Home to the Elves, the Golden Forest has failed to let anyone down! The Elves live in their treehouses and hey protect wildlife from harm, leaving them to live free among themselves. This mysterious and wonderous forest makes for a unforgettable experience.</h4>
+                <h4>Home to the Elves, the Golden Forest has failed to let anyone down! The Elves live in their treehouses and they protect wildlife from harm, leaving them to live free among themselves. This mysterious and wonderous forest makes for a unforgettable experience.</h4>
                 <ul id="addressDiv" style={{listStyleType: "none"}}>
                 <h3>Address:</h3>
                 <li>Caras Galadhon</li> 
