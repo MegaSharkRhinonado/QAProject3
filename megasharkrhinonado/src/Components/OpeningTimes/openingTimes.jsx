0b979c31e9b12bd1c9314a540ea5de2ff8ca9acb
Jesse <jesse.morgan@hotmail.co.uk>
2022-06-24 14:11:55 +0100

    MEG-9 #JM: added formatting

--- megasharkrhinonado/src/Components/OpeningTimes/openingTimes.jsx
+++ megasharkrhinonado/src/Components/OpeningTimes/openingTimes.jsx
@@ -1,12 +1,11 @@
-
 import OTCard01 from "./OpeningTimeCard/OTCard01";
-import OTCard02 from "./OpeningTimeCard/OTCard02";
 
 const OpeningTimes = () => {
     return ( 
         <>
-            <OTCard01 />
-            <OTCard02 />
+            <div className="content">
+                <OTCard01 />
+            </div>
         </>
      );
 }
