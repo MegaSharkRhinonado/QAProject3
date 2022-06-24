8d3bef47d39e4b004053720b0a27736cfe9f83da
elistribley <eli_stribley@hotmail.com>
2022-06-24 13:01:32 +0100

    MEG-55 #ES: Jira update for maps

--- megasharkrhinonado/src/Components/GettingThere/GettingThereComp/GTComp01.jsx
+++ megasharkrhinonado/src/Components/GettingThere/GettingThereComp/GTComp01.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-
+//We tried using other methods such as google-maps-react and react-leaflet, but Jesse found <iframe> allowing us to embed the LOTR interactive map.
 
 const GTComp01 = () => {
   return (
     <>
-    <iframe src="http://lotrproject.com/map/#zoom=3&lat=-1344&lon=1500&layers=BTTTTT" width="60%" height="300px" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    
+    <iframe src="http://lotrproject.com/map/#zoom=3&lat=-1344&lon=1500&layers=BTTTTT" width="60%" height="400px" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </>
   )
 }
