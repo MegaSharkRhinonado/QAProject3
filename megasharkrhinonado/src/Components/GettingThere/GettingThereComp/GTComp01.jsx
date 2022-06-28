05e243542579c85b866738433b1727462fec6710
elistribley <eli_stribley@hotmail.com>
2022-06-28 11:36:17 +0100

    MEG-55 #ES: iframe labeled Map, was giving warning back

--- megasharkrhinonado/src/Components/GettingThere/GettingThereComp/GTComp01.jsx
+++ megasharkrhinonado/src/Components/GettingThere/GettingThereComp/GTComp01.jsx
@@ -5,7 +5,7 @@ const GTComp01 = () => {
   return (
     <>
     
-    <iframe src="http://lotrproject.com/map/#zoom=3&lat=-1344&lon=1500&layers=BTTTTT" width="60%" height="400px" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <iframe title="Map" src="http://lotrproject.com/map/#zoom=3&lat=-1344&lon=1500&layers=BTTTTT" width="60%" height="400px" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </>
   )
 }
