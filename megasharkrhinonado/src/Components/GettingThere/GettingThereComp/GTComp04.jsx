a0e18a7191b25a901e7acc4743490b3aeda17d82
elistribley <eli_stribley@hotmail.com>
2022-06-28 11:49:02 +0100

    MEG-53 #ES: Given parking space notice on the bottom of getting there page

---
+++ megasharkrhinonado/src/Components/GettingThere/GettingThereComp/GTComp04.jsx
@@ -0,0 +1,11 @@
+const GTComp04 = () => {
+    return(
+        <>
+        <h4>Very limited parking spaces, we advise you travel here by horse!</h4>
+        <h4>We can't wait to see you!</h4>
+        
+        </>
+    )
+}
+
+export default GTComp04;
