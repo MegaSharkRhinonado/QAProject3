c0424432e7648142697da5614328826d729d118e
elistribley <eli_stribley@hotmail.com>
2022-06-28 11:59:25 +0100

    MEG-51 #ES: Added carriage transport links to getting there page

--- megasharkrhinonado/src/Components/GettingThere/GettingThereComp/GTComp04.jsx
+++ megasharkrhinonado/src/Components/GettingThere/GettingThereComp/GTComp04.jsx
@@ -1,9 +1,19 @@
 const GTComp04 = () => {
     return(
         <>
+        <div>
         <h4>Very limited parking spaces, we advise you travel here by horse!</h4>
+        <h4>There are plenty of public transport carriage so you can visit us safely. Carriages stop are at the following:</h4>
+        <ul>
+        <h5>Michel Delving</h5>
+        <h5>Bree</h5>
+        <h5>Rivendell</h5>
+        <h5>Caras Galadhon</h5>
+        <h5>Helm's Deep</h5>
+        <h5>Minas Tirith</h5>
         <h4>We can't wait to see you!</h4>
-        
+        </ul>
+        </div>
         </>
     )
 }
