db5677671628582dd8d1eb018b70bd2ffdae5b60
Jesse <jesse.morgan@hotmail.co.uk>
2022-06-29 11:26:21 +0100

    MEG-97 #JM: updated logo + reviewed homepage title

--- megasharkrhinonado/src/Components/Homepage/HomePageComp/homeCard01.jsx
+++ megasharkrhinonado/src/Components/Homepage/HomePageComp/homeCard01.jsx
@@ -1,16 +1,14 @@
 
 // change source to the image needed
-import image01 from '../../../Media/HomePage/StretchCinema.png';
+import image01 from '../../../Media/Nav/LOGO.png';
 
 const homeCard01 = () => {
     return ( 
         <>
-        <center style={{
-            margin: '1% 0px 0px 0%',
-
-          }}>
-        <img src={image01} alt="CinemaBanner" width="90%" height="200px"/>
-        </center>
+          <div className="homepage">
+            <img src={image01} alt="CinemaBanner" height="180vh"/>
+            <p><strong>Cinemas</strong></p>
+          </div>
        </>
      );
 }
