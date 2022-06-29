0e2f9fa29970a5e07d207f9a0d0be946a6beff32
Jesse <jesse.morgan@hotmail.co.uk>
2022-06-29 09:53:12 +0100

    MEG-83 #JM: Added contact link + formatting

--- megasharkrhinonado/src/Components/About/about.jsx
+++ megasharkrhinonado/src/Components/About/about.jsx
@@ -1,11 +1,12 @@
 import ACard01 from "./AboutCards/ACard01";
+import { Link } from 'react-router-dom'
 
 const About = () => {
     return ( 
         <>
             <div className="content">
                 <ACard01 />
-   
+                <div id="contact"><Link to="/ContactUs"><strong>Contact Us</strong></Link></div>
             </div> 
         </>
      );
