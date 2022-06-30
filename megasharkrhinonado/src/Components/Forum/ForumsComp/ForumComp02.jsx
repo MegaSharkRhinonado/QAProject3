ba3e15a99d5f2c132b9db3fa485ab04fa4e45f3c
Matthew Duck <MatthewDDuck@googlemail.com>
2022-06-30 11:03:45 +0100

    MEG-54 #Created axios methods to link forums to the forum database and properly display got data

--- megasharkrhinonado/src/Components/Forum/ForumsComp/ForumComp02.jsx
+++ megasharkrhinonado/src/Components/Forum/ForumsComp/ForumComp02.jsx
@@ -1,29 +1,67 @@
-const ForumComp02 = () => {
+import axios from "axios";
+import React, { useEffect, useState, useRef } from "react";
+
+const ForumComp02 = (titles) => {
+
+    const [movieTitle, setTitle] = useState(titles);
+    const [titleSelected, setTitleSelected] = useState("");
+
+    const inputName = useRef();
+    const inputComment = useRef();
+    const inputRating = useRef();
+
+    console.log(movieTitle)
+
+    const handleMovieChange = (event) => {
+        setTitleSelected(previosState => {
+            return previosState = event.target.value;
+        })
+    }
+
+    const newReview = () => {
+        axios.post('http://localhost:3000/bookings/put/' + titleSelected, {
+            username: inputName,
+            comment: inputComment,
+            numberRating: inputRating
+        })
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
     return (
         <>
-            <div className="board">
+            <div name="board">
                 <h2>Discussion Board</h2>
                 <div>
-                    <label>Movie Title: </label> <input type="text" id="topic" placeholder="Lord Of The Rings: Two Towers" style={{ maxWidth: "250px", width: "100%" }}/>
+                    <label>Title: </label><select id="movieList" onChange={handleMovieChange}>
+                        {
+                            movieTitle.data?.map((entry, i) => (
+                                <option key={i} value={entry}>{entry}</option>
+                            ))}
+                    </select>
                 </div>
                 <div>
-                    <label>Name: </label><input type="text" id="Name Of User" placeholder="Bimbo Bobbins" style={{ maxWidth: "250px", width: "100%" }}/>
+                    <label>Name: </label><input type="text" id="Name Of User" placeholder="Bimbo Bobbins" style={{ maxWidth: "250px", width: "100%" }} ref={inputName} />
                 </div>
                 <div>
-                    <label for="Rating">Rating: </label>
-                        <select name="Rating" id="Rating" style={{ maxWidth: "40px", width: "100%" }}>
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                        </select>
+                    <label>Rating: </label>
+                    <select name="Rating" id="Rating" style={{ maxWidth: "40px", width: "100%" }}>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                    </select>
                 </div>
 
                 <div>
-                    <textarea rows="10" cols="50" type="text" class="CEN" id="creatureNotes" placeholder="This film was absolutly amazing"/>
+                    <textarea rows="10" cols="50" type="text" id="creatureNotes" placeholder="This film was absolutly amazing" ref={inputComment} />
                 </div>
-                <button id="createObjBtn">Submit</button>
+                <button id="createObjBtn" onClick={newReview}>Submit</button>
             </div>
         </>
     );
--- megasharkrhinonado/src/Components/Forum/forums.jsx
+++ megasharkrhinonado/src/Components/Forum/forums.jsx
@@ -1,17 +1,70 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import ForumCard01 from "./ForumsCard/ForumCard01";
 import ForumCard02 from "./ForumsCard/ForumCard02";
+import ForumComp02 from "./ForumsComp/ForumComp02";
+import CommentCard from "./ForumsComp/CommentCard"
 
 const Forums = () => {
+
+    const [movies, setMovies] = useState([]);
+    const [reviews, setReviews] = useState(movies);
+    const [titles, setTitles] = useState([]);
+    const testArray = ["1", "2", "3"];
+
+    const getMovie = () => {
+        axios.get("http://localhost:3000/movies/getAll")
+            .then(response => {
+                setMovies(response.data)
+            }).catch((exception) => {
+                console.log(exception);
+            });
+    }
+
+    const getReview = () => {
+        let selected = reviews
+        for (let i = 1; i <= reviews; i++) {
+            if (!reviews[i].numberRating) {
+                selected.push(i);
+            }
+        }
+        setReviews(selected);
+    }
+
+    const getTitle = () => {
+        let selected = [];
+        movies.map(data => (
+            selected.push(data.movieTitle)
+        ))
+        console.log("Selected:" + selected)
+        setTitles(selected)
+    }
+
+    useEffect(() => {
+        getMovie();
+        getReview();
+        getTitle();
+        console.log("titles:" + titles)
+    }, []);
+
     return (
         <>
             <div className="content">
                 <table>
-                    <tr>
-                        <ForumCard01 />
-                    </tr>
-                    <td>
-                        <ForumCard02 />
-                    </td>
+                    <tbody>
+                        <tr>
+                            <ForumCard01 />
+                        </tr>
+                        <ForumComp02 data={testArray} />
+                        <td>
+                            {
+                                reviews.map(movie => (
+                                    <CommentCard data={movie} />
+                                ))
+                            }
+
+                        </td>
+                    </tbody>
                 </table>
             </div>
         </>
