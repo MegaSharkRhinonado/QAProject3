e19fbda23c46c8ab3b547e5b978cbcb4b1263392
Jesse <jesse.morgan@hotmail.co.uk>
2022-07-01 14:06:00 +0100

    MEG-44 #JM:forum page reformatting

--- megasharkrhinonado/src/Components/Forum/ForumsComp/Review.jsx
+++ megasharkrhinonado/src/Components/Forum/ForumsComp/Review.jsx
@@ -2,24 +2,24 @@ const Review = (data) => {
     
     return (
         <>
-        <div className="miniDiv">
-        <center>
-            <div style={{
-                width: '99.65%',
-                margin: '0px 0px 0px 0%',
-                border: '5px solid #555',
-            }}>
-                <div>
-                    <h3>{data.review.username}</h3>
-                </div>
-                <div>
-                    <h3>{data.review.numberRating}</h3>
-                </div>
-                <div>
-                    <h3>{data.review.comment}</h3>
-                </div>
-            </div>
-            </center>
+            <div className="miniDiv">
+                <center>
+                    <div style={{
+                        width: '99.65%',
+                        margin: '0px 0px 0px 0%',
+                        border: '5px solid #555',
+                    }}>
+                        <div>
+                            <h3>{data.review.username}</h3>
+                        </div>
+                        <div>
+                            <h3>{data.review.numberRating}</h3>
+                        </div>
+                        <div>
+                            <h3>{data.review.comment}</h3>
+                        </div>
+                    </div>
+                </center>
             </div>
 
         </>
