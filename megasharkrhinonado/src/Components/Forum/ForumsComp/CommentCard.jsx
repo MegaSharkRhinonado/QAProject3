f710be6a4a10eb44408f99c1bf73c72d66076aab
Matthew Duck <MatthewDDuck@googlemail.com>
2022-07-01 10:28:33 +0100

    MD: Fixed issue with displaying empty reviews by creating a function to find only non empty reviews as well as creating component to display reviews

--- megasharkrhinonado/src/Components/Forum/ForumsComp/CommentCard.jsx
+++ megasharkrhinonado/src/Components/Forum/ForumsComp/CommentCard.jsx
@@ -1,25 +1,23 @@
-const CommentCard = (data) => {
+import Review from "./Review";
+
+const CommentCard = ({ data }) => {
+
     return (
         <>
-        <center>
-            <div style={{
-                width: '99.65%',
-                margin: '0px 0px 0px 0%',
-                border: '5px solid #555',
-            }}>
-                <div>
-                    <center><h2>{data.data.movieTitle}</h2></center>
+            <center>
+                <div style={{
+                    width: '99.65%',
+                    margin: '0px 0px 0px 0%',
+                    border: '5px solid #555',
+                }}>
+                    <div>
+                        <center><h2>{data.movieTitle}</h2></center>
+                    </div>
+                    {
+                        data.movieReviews.map((review) => (
+                            <Review key={data._id} review={review} />
+                        ))}
                 </div>
-                <div>
-                    <h3>{data.username}</h3>
-                </div>
-                <div>
-                    <h3>{data.numberRating}</h3>
-                </div>
-                <div>
-                    <h3>{data.comment}</h3>
-                </div>
-            </div>
             </center>
 
         </>
